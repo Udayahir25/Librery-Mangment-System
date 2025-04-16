fb76b07067fa2455f171ae61cf90c2ae1b6659ed
Udayahir25 <udaynandaniya987@gmail.com>
2025-04-16 10:51:28 +0530

    db

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
-<?php
+<!-- <?php
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -11,4 +11,22 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+?> -->
+
+
+
+<?php
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12773489";
+$password = "YOUR_PASSWORD";  // Replace this with the password from your email
+$dbname = "sql12773489";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
 ?>
+
